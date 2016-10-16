ea3bf2de6e8f4b3b1dc90fccf40101b84042a3a4
d-smith <doug.smith.mail@gmail.com>
2016-10-16 10:22:00 -0700

    Refactored tests to do table driven test with subtests

--- atompub_test.go
+++ atompub_test.go
@@ -1,8 +1,11 @@
 package atompubsvc
 
 import (
+	"database/sql/driver"
 	"encoding/base64"
 	"encoding/xml"
+	"errors"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
@@ -11,142 +14,160 @@ import (
 	"net/http/httptest"
 	"testing"
 	"time"
-	"errors"
 )
 
-func TestRetrieveEvent(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+func TestRetrieve(t *testing.T) {
 
 	ts := time.Now()
-	rows := sqlmock.NewRows([]string{"event_time", "typecode", "payload"}).AddRow(
-		ts, "foo", []byte("yeah ok"))
 
-	mock.ExpectQuery("select event_time").WillReturnRows(rows)
-
-	eventHandler, err := NewEventRetrieveHandler(db, "myhost:12345")
-	assert.Nil(t, err)
-
-	router := mux.NewRouter()
-	router.HandleFunc("/notifications/{aggregate_id}/{version}", eventHandler)
-
-	r, err := http.NewRequest("GET", "/notifications/1234567/1", nil)
-	assert.Nil(t, err)
-	w := httptest.NewRecorder()
-
-	router.ServeHTTP(w, r)
-
-	//Validate status code
-	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
-
-	//Read the response
-	eventData, err := ioutil.ReadAll(w.Body)
-	assert.Nil(t, err)
-
-	//Validate content
-	var event EventStoreContent
-	err = xml.Unmarshal(eventData, &event)
-	if assert.Nil(t, err) {
-		assert.Equal(t, "1234567", event.AggregateId)
-		assert.Equal(t, "foo", event.TypeCode)
-		assert.Equal(t, 1, event.Version)
-		assert.Equal(t, base64.StdEncoding.EncodeToString([]byte("yeah ok")), event.Content)
-		assert.Equal(t, ts, event.Published)
+	var retrieveTests = []struct {
+		testName       string
+		nilDB          bool
+		aggregateId    string
+		version        string
+		expectedStatus int
+		colNames       []string
+		rowCols        []driver.Value
+		queryError     error
+		expectedEvent  *EventStoreContent
+	}{
+		{
+			"retrieve no error",
+			false,
+			"1234567",
+			"1",
+			http.StatusOK,
+			[]string{"event_time", "typecode", "payload"},
+			[]driver.Value{ts, "foo", []byte("yeah ok")},
+			nil,
+			&EventStoreContent{
+				AggregateId: "1234567",
+				TypeCode:    "foo",
+				Version:     1,
+				Content:     base64.StdEncoding.EncodeToString([]byte("yeah ok")),
+				Published:   ts,
+			},
+		},
+		{
+			"handler with nill db",
+			true,
+			"1234567",
+			"1",
+			http.StatusBadRequest,
+			[]string{},
+			[]driver.Value{},
+			nil,
+			nil,
+		},
+		{
+			"retrieve with malformed version",
+			false,
+			"1234567",
+			"x",
+			http.StatusBadRequest,
+			[]string{},
+			[]driver.Value{},
+			nil,
+			nil,
+		},
+		{
+			"retrieve with no rows found",
+			false,
+			"1234567",
+			"1",
+			http.StatusNotFound,
+			[]string{"event_time", "typecode", "payload"},
+			[]driver.Value{},
+			nil,
+			nil,
+		},
+		{
+			"retrieve with sql error",
+			false,
+			"1234567",
+			"1",
+			http.StatusInternalServerError,
+			[]string{"event_time", "typecode", "payload"},
+			[]driver.Value{},
+			errors.New("kaboom"),
+			nil,
+		},
 	}
 
-	//Validate cache headers
-	cc := w.Header().Get("Cache-Control")
-	assert.Equal(t, "max-age=2592000", cc)
+	for _, test := range retrieveTests {
+		t.Run(test.testName, func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
 
-	etag := w.Header().Get("ETag")
-	assert.Equal(t, "1234567:1", etag)
+			rows := sqlmock.NewRows(test.colNames)
+			if len(test.rowCols) > 0 {
+				t.Log("add test row data")
+				rows = rows.AddRow(test.rowCols...)
+			}
 
-	//Validate content type
-	assert.Equal(t, "application/xml", w.Header().Get("Content-Type"))
+			var query *sqlmock.ExpectedQuery
+			if len(test.colNames) > 0 {
+				query = mock.ExpectQuery("select")
+				query = query.WillReturnRows(rows)
+			}
 
-}
+			if test.queryError != nil {
+				if query == nil {
+					query = mock.ExpectQuery("select")
+				}
+				query = query.WillReturnError(test.queryError)
+			}
 
-func TestRetrieveEventParamValidation(t *testing.T) {
+			var eventHandler func(http.ResponseWriter, *http.Request)
+			if test.nilDB == false {
+				eventHandler, err = NewEventRetrieveHandler(db, "myhost:12345")
+				assert.Nil(t, err)
+			} else {
+				eventHandler, err = NewEventRetrieveHandler(nil, "myhost:12345")
+				assert.NotNil(t, err)
+				return
+			}
 
-	//Can't instantiated without a database DB instance
-	_,err := NewEventRetrieveHandler(nil, "x")
-	if assert.NotNil(t, err) {
-		assert.Equal(t, ErrBadDBConnection, err)
+			router := mux.NewRouter()
+			router.HandleFunc("/notifications/{aggregate_id}/{version}", eventHandler)
+
+			r, err := http.NewRequest("GET", fmt.Sprintf("/notifications/%s/%s", test.aggregateId, test.version), nil)
+			assert.Nil(t, err)
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, r)
+
+			//Validate status code
+			assert.Equal(t, test.expectedStatus, w.Result().StatusCode)
+
+			if test.expectedEvent != nil {
+				//Read the response
+				eventData, err := ioutil.ReadAll(w.Body)
+				assert.Nil(t, err)
+
+				var event EventStoreContent
+				err = xml.Unmarshal(eventData, &event)
+				if assert.Nil(t, err) {
+					assert.Equal(t, test.expectedEvent.AggregateId, event.AggregateId)
+					assert.Equal(t, test.expectedEvent.TypeCode, event.TypeCode)
+					assert.Equal(t, test.expectedEvent.Version, event.Version)
+					assert.Equal(t, test.expectedEvent.Content, event.Content)
+					assert.Equal(t, test.expectedEvent.Published, event.Published)
+				}
+
+				//Validate cache headers
+				cc := w.Header().Get("Cache-Control")
+				assert.Equal(t, "max-age=2592000", cc)
+
+				etag := w.Header().Get("ETag")
+				assert.Equal(t, "1234567:1", etag)
+
+				//Validate content type
+				assert.Equal(t, "application/xml", w.Header().Get("Content-Type"))
+			}
+		})
 	}
-
-	//Should get an error with invalid version
-	db, _, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	eventHandler,err := NewEventRetrieveHandler(db, "x")
-	assert.Nil(t,err)
-
-	router := mux.NewRouter()
-	router.HandleFunc("/notifications/{aggregate_id}/{version}", eventHandler)
-
-	r, err := http.NewRequest("GET", "/notifications/1234567/foobar", nil)
-	assert.Nil(t, err)
-	w := httptest.NewRecorder()
-
-	router.ServeHTTP(w, r)
-
-	assert.Equal(t, http.StatusBadRequest, w.Result().StatusCode)
-
-}
-
-func TestRetrieveEventNoSuchEvent(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	rows := sqlmock.NewRows([]string{"event_time", "typecode", "payload"})
-
-	mock.ExpectQuery("select event_time").WillReturnRows(rows)
-
-	eventHandler, err := NewEventRetrieveHandler(db, "myhost:12345")
-	assert.Nil(t, err)
-
-	router := mux.NewRouter()
-	router.HandleFunc("/notifications/{aggregate_id}/{version}", eventHandler)
-
-	r, err := http.NewRequest("GET", "/notifications/1234567/1", nil)
-	assert.Nil(t, err)
-	w := httptest.NewRecorder()
-
-	router.ServeHTTP(w, r)
-
-	//Validate status code
-	assert.Equal(t, http.StatusNotFound, w.Result().StatusCode)
-}
-
-func TestRetrieveEventQueryError(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	mock.ExpectQuery("select event_time").WillReturnError(errors.New("boom"))
-
-	eventHandler, err := NewEventRetrieveHandler(db, "myhost:12345")
-	assert.Nil(t, err)
-
-	router := mux.NewRouter()
-	router.HandleFunc("/notifications/{aggregate_id}/{version}", eventHandler)
-
-	r, err := http.NewRequest("GET", "/notifications/1234567/1", nil)
-	assert.Nil(t, err)
-	w := httptest.NewRecorder()
-
-	router.ServeHTTP(w, r)
-
-	//Validate status code
-	assert.Equal(t, http.StatusInternalServerError, w.Result().StatusCode)
 }
